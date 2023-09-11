3ddea52a91b0db983e75418f7773d61a08786668
Marco-Thiele <125655717+Marco-Thiele@users.noreply.github.com>
2023-09-11 12:10:37 +0200

    second push

--- dialog-add-user/dialog-add-user.component.ts
+++ dialog-add-user/dialog-add-user.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { User } from 'src/models/user.class';
+import { inject } from '@angular/core';
+import { Firestore, collectionData, collection, doc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { addDoc } from 'firebase/firestore';
+import { getFirestore } from "firebase/firestore";
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -9,13 +14,51 @@ import { User } from 'src/models/user.class';
 export class DialogAddUserComponent {
   user: User = new User();
   birthdate: Date | undefined;
+  //item$: Observable<any[]>
+  firestore: Firestore = inject(Firestore);
+  //items;
+
+  constructor() {
+   // const itemCollection = collection(this.firestore, 'users');
+   // this.item$ = collectionData(itemCollection);
+    // this.items = this.item$.subscribe((list) => {
+    //   list.forEach(element => {
+    //     console.log(element)
+    //   });
+    // })
+
+  }
+
   onNoClick(){
 
   }
 
 
-  saveUser(){
+  async saveUser(){
     this.user.birthDate = this.birthdate?.getTime();
     console.log('user', this.user );
+
+     await addDoc(this.getUsersRef(), this.user.toJSON())
+     .catch((err) => {
+      console.error(err)
+     })
+    .then((result:any) => {
+      console.log('Adding user finished', result)
+    })
+
+    // this.firestore.collection(this.firestore, 'users')
+    // .add(this.user.toJSON())
+    // .than((result:any) => {
+    //   console.log('Adding user finished', result)
+    // })
+  }
+
+
+  getUsersRef(){
+    return collection(this.firestore, 'users');
+  }
+
+  getSingelRef(colId:string, docId:string) {
+    return doc(collection(this.firestore, colId), docId)
   }
 }
